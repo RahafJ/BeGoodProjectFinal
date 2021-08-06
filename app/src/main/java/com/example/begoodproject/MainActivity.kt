2c2977457a9d3baf77620518c4633b00197c5b6e
RahafJarrah <rahaf1jarrah@gmail.com>
2021-08-06 15:23:01 +0300

    SplashScreen,, LogIn,, Sign Up Page

--- app/src/main/java/com/example/begoodproject/MainActivity.kt
+++ app/src/main/java/com/example/begoodproject/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.begoodproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_BeGoodProject)
         setContentView(R.layout.activity_main)
+
+
+        btnSignUp.setOnClickListener{
+            Intent(this, SignUp::class.java).also{
+                startActivity(it)
+            }
+
+        }
     }
 }
+
---
+++ app/src/main/java/com/example/begoodproject/SignUp.kt
@@ -0,0 +1,14 @@
+package com.example.begoodproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_BeGoodProject)
+        setContentView(R.layout.sign_up)
+
+
+    }
+}
