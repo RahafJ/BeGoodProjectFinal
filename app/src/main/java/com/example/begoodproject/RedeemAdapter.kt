b87a981149618a56cec28fb70d42c9c171e85873
RahafJarrah <rahaf1jarrah@gmail.com>
2021-08-14 11:58:30 +0300

    SplashScreen,, LogIn,, Sign Up Page,, Home Page,,, navigation Drawer,, bills Layout ,,,, User sign up and log in,,, advice and about us Log in Sign up with firebase is DONE ,,, Trying in shop activity view

--- app/src/main/java/com/example/begoodproject/RedeemAdapter.kt
+++ app/src/main/java/com/example/begoodproject/RedeemAdapter.kt
@@ -1,6 +1,26 @@
 package com.example.begoodproject
 
-class RedeemAdapter (
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
 
-        ){
+class RedeemAdapter (
+        private var redeemList:MutableList<ItemRedeem>
+
+        ):RecyclerView.Adapter<RedeemAdapter.RedeemViewHolder>(){
+                class RedeemViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RedeemViewHolder {
+                TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RedeemViewHolder, position: Int) {
+                TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+                TODO("Not yet implemented")
+        }
+
 }
